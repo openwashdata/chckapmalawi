efb3de89cffaf8517952dfd98fe62361ddb0c62c
Lars Schöbitz <lars@lse.de>
2023-10-25 10:15:05 +0200

    document data set using roxygen comments

---
+++ R/chckapmalawi.R
@@ -0,0 +1,14 @@
+#' Community Health Club Knowledge Attitude Practice Survey Malawi
+#'
+#' A survey conducted by BASEflow for Waste Advisers, Malawi. The survey was
+#' implemented in 20XX.
+#'
+#' @format A tibble with 500 rows and 4 variables:
+#' \describe{
+#'   \item{village}{ Village in Malawi.}
+#'   \item{gender}{ Gender of the respondent.}
+#'   \item{age}{ Age of the respondent.}
+#'   \item{water_pay}{ Question: Do you believe you should pay for your water?}
+#' }
+#'
+"chckapmalawi"
